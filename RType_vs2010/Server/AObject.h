1ff02092dd98c4617a177a3eff90d11d5a0217cc
GwenaelD <gwenael.derval@gmail.com>
2013-11-13 17:05:06 +0100

    Coorection Object, désolé Hugo.

--- RType_vs2010/Server/AObject.h
+++ RType_vs2010/Server/AObject.h
@@ -8,15 +8,21 @@ public:
 	int getPosY() const;
 	int getVecVx() const;
 	int getVecVy() const;
+	int getWidth() const;
+	int getHeight() const;
+	void	setWidth(int w);
+	void	setHeight(int w);
 	void setPosX(int x);
 	void setPosY(int y);
 	void setVecVx(int Vx);
 	void setVecVy(int Vy);
-
+	bool getCollision(AObject *obj);
 private:
-	int	x;
-	int	y;
-	int Vx;
-	int Vy;
+	int	_x;
+	int	_y;
+	int _Vx;
+	int _Vy;
+	int _width;
+	int _height;
 };
 
